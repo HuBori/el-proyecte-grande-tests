405e1cc2b6ab5d6aa2d1ef1fadcfe1dc4af3841b
cSora <somfaida@gmail.com>
2021-12-09 10:58:51 +0100

    feat: implement step logics

--- src/test/java/com/example/steps/ListAllEmployeesSteps.java
+++ src/test/java/com/example/steps/ListAllEmployeesSteps.java
@@ -5,74 +5,93 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class ListAllEmployeesSteps {
     ListAllEmployeeUtility utility = new ListAllEmployeeUtility();
     int numberOfEmployees;
 
-    @Given("I am on list all employees page")
-    public void iAmOnListAllEmployeesPage() {
-        utility.openPage();
-    }
-
     @Given("I have x of employees")
     public void iHaveNumberOfEmployees() {numberOfEmployees = utility.getEmployeeNumber();}
 
-    @Then("I should see x as total number of employees")
-    public void iShouldSeeAsTotalNumberOfEmployees() {
-
+    @Then("I should see {string} as total number of employees")
+    public void iShouldSeeAsTotalNumberOfEmployees(String envVariableName) {
+        int employeeNumber = Integer.parseInt(System.getenv(envVariableName));
+        Assertions.assertEquals(employeeNumber, numberOfEmployees);
     }
 
     @When("I search for {string} in the search bar")
-    public void iSearchForInTheSearchBar(String employeeName) {
-
+    public void iSearchForNameInTheSearchBar(String employeeName) {
+        utility.searchForName(employeeName);
     }
 
 
     @Then("I should see the search results {string} {string}")
     public void iShouldSeeTheSearchResults(String employeeName, String status) {
+        switch (status) {
+            case "true":
+                Assertions.assertTrue(utility.isEmployeePresentInTable(employeeName));
+                break;
+            case "false":
+                Assertions.assertFalse(utility.isEmployeePresentInTable(employeeName));
+                break;
+        }
     }
 
-    @And("{int} is bigger than {string}")
-    public void isBiggerThan(int numberOfEmployees, String employeePerPage) {
-        
+    @And("{string} is bigger than {string}")
+    public void isBiggerThan(String numberOfEmployees, String employeePerPage) {
+        Assertions.assertTrue(utility.compareNumbers(Integer.parseInt(System.getenv(numberOfEmployees)), Integer.parseInt(employeePerPage)));
     }
 
     @Then("next button should be active")
     public void nextButtonShouldBeActive() {
-        utility.isNextBtnActive();
+        Assertions.assertTrue(utility.isNextBtnActive());
     }
 
     @When("I press next button")
     public void iPressNextButton() {
-        
+        utility.clickNextBtn();
     }
 
     @Then("table content should change")
     public void tableContentShouldChange() {
-
+        Assertions.assertTrue(utility.tableContentChanged());
     }
 
     @Then("prev button should be active")
     public void prevButtonShouldBeActive() {
-
+        Assertions.assertTrue(utility.isPrevBtnActive());
     }
 
     @When("I press prev button")
     public void iPressPrevButton() {
-
+        utility.clickPrevBtn();
     }
 
-    @And("employee {string} is present in the list")
+    @Given("employee {string} is present in the list")
     public void employeeIsPresentInTheList(String employeeName) {
-
+        utility.setAndCheckEmployeeElement(employeeName);
     }
 
     @When("I click on {string}")
     public void iClickOn(String employeeName) {
+        utility.clickOn(employeeName);
     }
 
-    @Then("I should be redirected to {string} details")
-    public void iShouldBeRedirectedToDetails(String employeeName) {
+    @Then("I should be redirected to {string} and displayed {string} is valid")
+    public void iShouldBeRedirectedToDetails(String url, String name) {
+        String expectedURL = url;
+        Assertions.assertEquals(expectedURL, utility.getCurrentUrl());
+        Assertions.assertTrue(utility.validateData(name));
+    }
+
+    @When("i click on all employees")
+    public void iClickOnAllEmployees() {
+        utility.navigateToAllEmployee();
+    }
+
+    @Then("user is promted to log in")
+    public void userIsPromtedToLogIn() {
+        utility.logIn();
     }
 }
